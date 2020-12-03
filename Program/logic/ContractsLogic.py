726574e3fe061c608da9e40a0295f2c709df7fef
Fannart20 <70716739+Fannart20@users.noreply.github.com>
2020-12-03 21:46:36 +0000

    Add files via upload

--- Program/logic/ContractsLogic.py
+++ Program/logic/ContractsLogic.py
@@ -14,5 +14,28 @@ class ContractLogic:
         for item in self.data.get_contracts():
             temporary_list = item.get_customer().split()
     
-    def create_new_contract(self, string):
-        return "d"
+    def create_new_contract(self, list):      #Vantar betri útfærslu?
+        self.data.add_contract(list[0], list[1], list[2], list[3], list[4], self.add_contract_id())
+
+    def add_contract_id(self):
+        return (len(self.all_contracts()) + 1)
+
+    def search_contracts_by_customer(self, string):
+        result_list = []
+        for contract in self.all_contracts():
+            if contract.get_customer().lower() == string.lower():
+                result_list.append(contract)
+        if result_list:
+            return result_list
+        
+    def search_contracts_by_vin(self, string):
+        result_list = []
+        for contract in self.all_contracts():
+            if contract.get_vin().lower() == string.lower():
+                result_list.append(contract)
+        if result_list:
+            return result_list
+
+    def search_contracts_by_id(self, string):
+        for contract in self.all_contracts():
+            if contract.get
