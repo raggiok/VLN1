e7f5077b25e7615f9c6f8c6ae44ea80abcda2327
Fannart20 <70716739+Fannart20@users.noreply.github.com>
2020-12-03 21:45:42 +0000

    Add files via upload

---
+++ Program/models/contracts.py
@@ -0,0 +1,34 @@
+class Contract:
+    def __init__(self, customer, vehicle_vin, start_date, end_date, country, contract_id):
+        self.customer = customer
+        self.vehicle_vin = vehicle_vin
+        self.start_date = start_date
+        self.end_date = end_date
+        self.country = country
+        self.identification_number = str(contract_id)
+        
+    def change_customer_name(self, new_customer_name):
+        self.customer = new_customer_name
+
+    def change_vehicle(self, new_vehicle_vin):
+        self.vehicle_vin = new_vehicle_vin
+
+    def change_start_date(self, new_start_date, new_end_date):
+        self.start_date = new_start_date
+        self.end_date = new_end_date
+
+    def change_country(self, new_country_name):
+        self.country = new_country_name
+
+    def get_customer(self):
+        return self.customer
+    
+    def get_vin(self):
+        return self.vehicle_vin
+
+    def get_identification_number(self):
+        return self.identification_number
+        
+    def __str__(self):
+        contract_string = f'{self.identification_number} {self.customer} {self.vehicle_vin} {self.start_date} {self.end_date} {self.country}'
+        return contract_string
