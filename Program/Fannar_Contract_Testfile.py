98a19742ff4e3f686d816c4406e7f135e1ea4768
Fannar <fannart20@ru.is>
2020-12-05 20:54:40 +0000

    Created a logic API

--- Program/Fannar_Contract_Testfile.py
+++ Program/Fannar_Contract_Testfile.py
@@ -1,16 +1,9 @@
-<<<<<<< HEAD
 from data.ContractsDatabase import ContractData
 from models.contracts import Contract
 from logic.ContractsLogic import ContractLogic
 from ui.ContractsUI import ContractUI
 
 #logic = ContractLogic()
-=======
-from data.ContractsDatabase import ContractData
-from models.contracts import Contract
-from logic.ContractsLogic import ContractLogic
-from ui.ContractsUI import ContractUI
-
-#logic = ContractLogic()
->>>>>>> d34924f5aaaf3e21e1201376dcfcab0c8cdb8067
-data_main = ContractUI()
+#data_main = ContractUI()
+alist = ["faa-lala","lklkl","rtrtrt","pepepe"]
+print(*alist)
--- Program/data/ContractsDatabase.py
+++ Program/data/ContractsDatabase.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import csv
 import os
 from models.contracts import Contract
@@ -37,44 +36,4 @@ class ContractData:
                         confirmation = True
                         continue
                     writer.writerow(row)
-=======
-import csv
-import os
-from models.contracts import Contract
-
-class ContractData:
-    def __init__(self):
-        print("yolo")
-
-    def get_contracts(self):
-        contract_list = []
-        with open('data/contracts.csv', newline='', encoding="utf-8") as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                contract = Contract(row['customer'], row["vin"], row["start_date"],row["end_date"],row["country"],row["contract_id"])
-                contract_list.append(contract)
-        return contract_list
-
-    def add_contract(self, customer, vin, start_date, end_date, country, contract_id):
-        with open('data/contracts.csv', 'a', newline='', encoding="utf-8") as csvfile:
-            fieldnames = ['customer', 'vin', 'start_date', 'end_date', 'country', 'contract_id']
-            writer = csv.DictWriter(csvfile, delimiter=',', fieldnames=fieldnames)
-            writer.writerow({'customer': customer, 'vin': vin, 'start_date': start_date, 'end_date': end_date, 'country': country, 'contract_id': contract_id})
-
-    def del_contract(self, new_id):
-        temporary_list = []
-        confirmation = False
-        with open('data/contracts.csv', 'r', encoding="utf-8") as cvsfile:
-            for row in csv.DictReader(cvsfile):
-                temporary_list.append(row)
-            with open('data/contracts.csv', 'w', newline='', encoding="utf-8") as writecvsfile:
-                fieldnames = ['customer', 'vin', 'start_date', 'end_date', 'country', 'contract_id'] 
-                writer = csv.DictWriter(writecvsfile, delimiter=',', fieldnames=fieldnames)
-                writer.writeheader()
-                for row in temporary_list:
-                    if row['contract_id'] == new_id:
-                        confirmation = True
-                        continue
-                    writer.writerow(row)
->>>>>>> d34924f5aaaf3e21e1201376dcfcab0c8cdb8067
             return confirmation
