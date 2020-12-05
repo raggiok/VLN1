5a050d2a9dc5bee3311ab15f42fe8e05adca7f53
Fannart20 <70716739+Fannart20@users.noreply.github.com>
2020-12-05 17:40:09 +0000

    Add files via upload

---
+++ Program/ui/ContractsLogic.py
@@ -0,0 +1,48 @@
+from data.ContractsDatabase import ContractData
+from models.contracts import Contract
+class ContractLogic:
+
+    def __init__(self):
+        self.data = ContractData()
+
+    def all_contracts(self):
+        return self.data.get_contracts()
+
+    def search_contracts_by_name(self, name):
+        ret_string = ""
+        temporary_list = []
+        for item in self.data.get_contracts():
+            temporary_list = item.get_customer().split()
+    
+    def create_new_contract(self, list):      #Vantar betri útfærslu?
+        self.data.add_contract(list[0], list[1], list[2], list[3], list[4], self.add_contract_id())
+
+    def add_contract_id(self):
+        return (len(self.all_contracts()) + 1)
+
+    def search_contracts_by_customer(self, string):
+        result_list = []
+        for contract in self.all_contracts():
+            if contract.get_customer().lower() == string.lower():
+                result_list.append(contract)
+        if result_list:
+            return result_list
+        
+    def search_contracts_by_vin(self, string):
+        result_list = []
+        for contract in self.all_contracts():
+            if contract.get_vin().lower() == string.lower():
+                result_list.append(contract)
+        if result_list:
+            return result_list
+
+    def search_contracts_by_id(self, string):
+        match = ""
+        for contract in self.all_contracts():
+            if contract.get_vin() == string:
+                match = contract
+        return match
+
+    def delete_contract(self, string):
+        confirmation = self.data.del_contract(string)
+        return confirmation
