465111df9331c831357c88a2d9fe1c1739ae37e5
unknown <raggiok@gmail.com>
2020-12-06 20:03:12 +0000

    Adding Invoice Class

---
+++ Program/models/Invoice.py
@@ -0,0 +1,15 @@
+class Invoice():
+
+    def __init__(self, unique_id, customer_unique_id, vehicle_unique_id, rate, total_days, _total_price="", invoice_type="Debit", state="Active", ):
+        self.unique_id = unique_id
+        self.vehicle_unique_id = vehicle_unique_id
+        self.customer_unique_id = customer_unique_id
+        self.rate = rate
+        self.total_days = total_days
+        self._total_price = self.calculate_price()
+        self.invoice_type = invoice_type #Can be Debit or Credit
+        self.state = state #Is active but if deleted becomes "Inactive"
+
+    def calculate_price(self):
+        total_price = int(self.rate) * int(self.total_days)
+        return total_price
