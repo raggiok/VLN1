2e15719c104600e4f6cfab47977f42e8e521b6a2
Fannar <fannart20@ru.is>
2020-12-09 19:35:44 +0000

    update

--- Program/Fannar_Contract_Testfile.py
+++ Program/Fannar_Contract_Testfile.py
@@ -5,7 +5,8 @@ t1 = date(year = 18, month = 7, day = 23)
 #t2 = date(year = 2018, month = 7, day = 23)
 
 
-print(LogicAPI().create_new_contract(["John Smith", "16", "13.11.20", "14.11.20","Nuk", "Jón", "1500", "09.12.20"]))
+print(LogicAPI().create_new_contract(["John Smith", "16", "15.11.20", "16.11.20","Nuuk", "Jón", "1500", "09.12.20"]))
 
+#ContractUI()
 
 a = 5 > 7
--- Program/logic/ContractsLogic.py
+++ Program/logic/ContractsLogic.py
@@ -26,11 +26,11 @@ class ContractLogic:
     def create_contract(self, a_list):
         new_contract = Contract(self.data.new_contract_id() , *a_list)
         if not self.check_availability(new_contract):
-            return "Vehicle unavailable at that time."
+            return "\n*****Vehicle unavailable at that time.*****"
         #elif not self.check_license(new_contract):
-        #    return "Inadequate license type."
+        #    return "*****Inadequate license type.*****"
         elif not self.check_country(new_contract):
-            return "Vehicle position and contract destination differ."
+            return "\n*****Vehicle position and contract destination differ.*****"
         else:
             new_contract.end_date = self.change_to_string(new_contract.end_date)
             new_contract.start_date = self.change_to_string(new_contract.start_date)
@@ -114,7 +114,7 @@ class ContractLogic:
         for vehicle in search_list:
             if vehicle.unique_id == ID:
                 retList.append(vehicle)
-        return self.no_match_found(retList)
+        return retList
 
     def customer_by_name(self,name):
         custs = self.data.get_customers()
