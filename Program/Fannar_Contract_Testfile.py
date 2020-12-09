a4613dfc0deff70501206a441e2f35d47169e58c
Fannar <fannart20@ru.is>
2020-12-09 12:38:35 +0000

    jkl

--- Program/Fannar_Contract_Testfile.py
+++ Program/Fannar_Contract_Testfile.py
@@ -1,9 +1,4 @@
-from logic.logicAPI import LogicAPI
-from ui.UIMain import UIMain
-from models.contracts import Contract
+import datetime
 
 
-
-for emp in LogicAPI().all_contracts():
-    print(emp)
-#print(LogicAPI().search_employees_by_role("lala"))
+print(datetime.date())
